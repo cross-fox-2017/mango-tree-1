41ea4ff5c18810456a9a34f5ba48364554b934b7
Irsan Sebastian <email": null>
2017-01-07 14:05:49 +0700

    First Commit

--- mango_tree_1.js
+++ mango_tree_1.js
@@ -1,32 +1,68 @@
 "use strict"
 
 class MangoTree {
-  
+
   // Initialize a new MangoTree
   constructor() {
+        this._umur = 0;
+        this._tinggi = getRandomHeight();
+        this._qty = 0;
+        this._health = true;
   }
 
-  // Get current states here
+  //Get current states here
+
 
   // Grow the tree
-  grow() {
+  grow(){
+
+    this._umur++;
+
+      if(this._umur  == 20)
+      {
+        this._health = false;
+
+      }else{
+
+          this._tinggi += getRandomHeight();
+      }
+
   }
 
   // Produce some mangoes
   produceMangoes() {
+
+      return this._qty = getRandomFruits();
   }
 
   // Get some fruits
   harvest() {
+
+      return this.produceMangoes();
   }
 }
 
-class Mango {
-  // Produce a mango
-  constructor() {
-  }
+
+function getRandomFruits() {
+      var num = Math.floor((Math.random() * 9) + 1);
+      return num;
 }
 
-function getRandomNumber() {
+function getRandomHeight(){
+
+      var num = Math.random() * 1;
+      return num;
+
 }
 
+var tree = new MangoTree()
+console.log(`The tree is alive! :smile:`)
+
+do {
+    tree.grow();
+    tree.produceMangoes();
+
+    console.log(`[Year ${tree._umur} Report] Height = ${tree._tinggi.toFixed(2)} Meter | Fruits Harvested = ${tree.harvest()}`);
+} while (tree._health != false)
+
+console.log(`The tree has met its end. :sad:`);
