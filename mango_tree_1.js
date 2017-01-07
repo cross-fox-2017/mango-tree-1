32fc4cae4e0c997f224fb9b1a217a55de99225f1
Mangku Widodo & Samuel Stauffer <pair+mangkuwi.samuel@hacktiv8.com>
2017-01-07 19:28:10 +0700

    all method done

--- mango_tree_1.js
+++ mango_tree_1.js
@@ -1,15 +1,31 @@
 "use strict"
 
 class MangoTree {
-  
+
   // Initialize a new MangoTree
   constructor() {
+    this._age = 0
+    this._height = 0
+    this._fruits = []
+    this._healty = true
   }
 
   // Get current states here
-
+  getAge(){
+    return this._age += 1
+  }
+  getHeight(){
+    return this.Height
+  }
+  getFruits(){
+    return this._fruits
+  }
+  getHealty(){
+    return this.healty
+  }
   // Grow the tree
   grow() {
+    
   }
 
   // Produce some mangoes
@@ -29,4 +45,3 @@ class Mango {
 
 function getRandomNumber() {
 }
-
