abf26babd0a37d46a07137c82c6960ab9b04963f
unknown <yaumashf@gmail.com>
2017-01-07 23:41:37 +0700

    done mango

--- mango_tree_1.js
+++ mango_tree_1.js
@@ -1,32 +1,50 @@
 "use strict"
 
 class MangoTree {
-  
-  // Initialize a new MangoTree
+
   constructor() {
+    this._umur=0;
+    this._tinggi=0;
+    this._fruits=0;
+    this._health=true;
   }
 
-  // Get current states here
-
-  // Grow the tree
   grow() {
+    var tinggi=Math.random();
+    this._umur++;
+    if(this._umur<=10){
+      this._tinggi+=tinggi;
+    }
+    if(this._umur==20) {
+      this._health=false;
+    }
   }
 
-  // Produce some mangoes
   produceMangoes() {
+    return this._fruits = getRandomNumber();
   }
 
-  // Get some fruits
   harvest() {
-  }
-}
-
-class Mango {
-  // Produce a mango
-  constructor() {
+    var jumlah = this.produceMangoes();
+    var good = Math.floor((Math.random() * jumlah) + 1);
+    var bad = jumlah - good;
+    return jumlah+' ('+good+' good, '+bad+' bad)';
   }
 }
 
 function getRandomNumber() {
+  var num = Math.floor((Math.random() * 9) + 1);
+  return num;
 }
 
+var tree = new MangoTree()
+console.log(`The tree is alive! :smile:`)
+
+do {
+    tree.grow();
+    tree.produceMangoes();
+
+    console.log(`[Year ${tree._umur} Report] Height = ${tree._tinggi.toFixed(2)} Meter | Fruits Harvested = ${tree.harvest()}`);
+} while (tree._health != false)
+
+console.log(`The tree has met its end. :sad:`);
