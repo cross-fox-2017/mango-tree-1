a791ebc5af903de679aed0555b3cff7e49ed9d74
Raditya <arya.raditya26@gmail.com>
2017-01-09 01:09:00 +0700

    Done all releases

--- mango_tree_1.js
+++ mango_tree_1.js
@@ -1,32 +1,47 @@
 "use strict"
 
 class MangoTree {
-  
+
   // Initialize a new MangoTree
   constructor() {
+    this.age = 0
+    this.height = 0
+    this.goodFruits = 0
+    this.badFruits = 0
+    this.status = true
   }
 
   // Get current states here
 
+  getFruits(){
+    this.goodFruits = Math.floor((Math.random() * 10))
+    this.badFruits = Math.floor((Math.random() * 10))
+  }
+
   // Grow the tree
   grow() {
+    this.age++
+    if (this.age < 17) {
+      this.height += Math.random()
+      this.status = true
+    }
+    else if (this.age == 30) {
+      this.status = false
+    }
   }
 
-  // Produce some mangoes
-  produceMangoes() {
-  }
-
-  // Get some fruits
-  harvest() {
-  }
 }
 
-class Mango {
-  // Produce a mango
-  constructor() {
-  }
+let harvest = new MangoTree()
+
+console.log(`The tree is alive! :smile`);
+
+while (harvest.status == true){
+  harvest.grow()
+  harvest.getFruits()
+  
+  console.log(`[Year ${harvest.age} Report] Height = ${harvest.height} m | Fruits harvested = ${harvest.goodFruits + harvest.badFruits} (${harvest.goodFruits} good, ${harvest.badFruits} bad)`);
+
 }
 
-function getRandomNumber() {
-}
-
+console.log(`The tree has met its end :sad:`);
