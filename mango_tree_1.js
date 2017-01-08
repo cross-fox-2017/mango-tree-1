ec177c387a908d65f8f3693cb91c4d37b3ba5e71
mrei91 <eryirawan91@gmail.com>
2017-01-09 02:00:31 +0700

    1st commit - on progress

--- mango_tree_1.js
+++ mango_tree_1.js
@@ -1,32 +1,66 @@
 "use strict"
 
 class MangoTree {
-  
+
   // Initialize a new MangoTree
   constructor() {
+    this._age     = 0;
+    this._height  = 0;
+    this._harvest = 0;
+    this._health  = true;
+    this._fruits  = [];
+
   }
 
   // Get current states here
+  getAge(){
+    return this._age;
+  }
+
+  getHeight(){
+    return this._height;
+  }
+
+  getHealth(){
+    return this._health;
+  }
+
+  getFruits(){
+    return this._fruits;
+  }
 
   // Grow the tree
   grow() {
+    this._age += 1;
+
+    if(this._age <= 10){
+      this._height += getRandomNumber();
+    }
+
+    if(this._age === 20){
+      this._health == false;
+    }
   }
 
   // Produce some mangoes
   produceMangoes() {
+
   }
 
   // Get some fruits
   harvest() {
+
   }
 }
 
 class Mango {
   // Produce a mango
   constructor() {
+
   }
 }
 
 function getRandomNumber() {
+  //untuk pertumbuhan dll
+  return Math.floor(Math.radom() * 9) + 1;
 }
-
