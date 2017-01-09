2d4a6db1ac3d1e8e6e259a5ca8a0054e2b31fc03
didietsuryadi <didietsuryadi@gmail.com>
2017-01-09 07:35:59 +0700

    mangotree finish

--- mango_tree_1.js
+++ mango_tree_1.js
@@ -1,32 +1,59 @@
 "use strict"
 
 class MangoTree {
-  
+
   // Initialize a new MangoTree
   constructor() {
+    this.Age = 0;
+    this.Height = getRandomNumber();
+    this.Quantity = 0;
+    this.Health = true;
   }
 
   // Get current states here
 
   // Grow the tree
   grow() {
+
+    this.Age++;
+
+    if(this.Age <= 10){
+      this.Height += getRandomNumber();
+    }
+    else if(this.Age == 20){
+      this.Health = false;
+    }
+
+
   }
 
   // Produce some mangoes
   produceMangoes() {
+    var fruits = Math.floor((Math.random() * 10)+ 1);
+    return this.Quantity = fruits;
+
   }
 
   // Get some fruits
   harvest() {
+    return this.produceMangoes();
   }
 }
 
-class Mango {
-  // Produce a mango
-  constructor() {
-  }
-}
 
 function getRandomNumber() {
+  var num = Math.random() * 1;
+  return num;
 }
 
+var tree = new MangoTree();
+console.log('The tree is alive! :smile:')
+
+do {
+  tree.grow();
+  tree.produceMangoes();
+
+  console.log(`[Year ${tree.Age}] Height = ${Math.floor(tree.Height)} Meter | Fruit Harvested = ${tree.harvest()}`);
+} while (tree.Health != false)
+
+console.log(`The tree has met its end. :sad:`);
